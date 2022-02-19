604151a31c63dbe3f44b228cfa4ef73a5af93027
siwoo kang <93714889+hexagonaldino@users.noreply.github.com>
2022-02-19 12:52:25 +0900

    Update DataType.java

--- JavaClass/CodeNam-A/01/DataType.java
+++ JavaClass/CodeNam-A/01/DataType.java
@@ -14,6 +14,27 @@ public class DataType {
 		System.out.println((int)2.9+1.8);
 		System.out.println((int)2.9+(int)1.8);
 
+		System.out.println("");
+		
+		int a = 10;
+		int c = 20;
+		
+		double a1 = 3.1;
+		double b1 = 2.1;
+		
+		System.out.println((int)c/a);
+		System.out.println((double)c/a);
+		
+		System.out.println("");
+		
+		System.out.println(b1/a1);
+		System.out.println(b1+a1);
+		
+		System.out.println("");
+		
+		System.out.println((float)(b1/a1));
+		System.out.println((double)(b1/a1));
+		
 	}
 
 }
