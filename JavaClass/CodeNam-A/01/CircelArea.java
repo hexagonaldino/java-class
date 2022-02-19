def07fa814a6e9ab27788a99185daef5d20b916e
siwoo kang <93714889+hexagonaldino@users.noreply.github.com>
2022-02-19 12:35:00 +0900

    Create CircelArea.java

---
+++ JavaClass/CodeNam-A/01/CircelArea.java
@@ -0,0 +1,14 @@
+
+public class CircelArea {
+
+	public static void main(String[] args) {
+		final double PI = 3.14;
+		
+		double radius = 10.0;
+		double cricleArea = radius*radius*PI;
+		
+		System.out.println("CriCleArea is : " + cricleArea);
+
+	}
+
+}
